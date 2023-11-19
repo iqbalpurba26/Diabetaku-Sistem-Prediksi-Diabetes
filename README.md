1095701e1c72a1eed3885a0200006cd6eddf6d61
iqbalpurba26 <myiqbalpurba@gmail.com>
2023-11-19 10:11:54 +0700

    add file and folder

---
+++ README.md
@@ -0,0 +1,31 @@
+# DiabetaKu : Aplikasi Sistem Prediksi Diabetes
+
+Diabetaku adalah Aplikasi berbasis website yang dapat digunakan untuk memprediksi berdasarkan parameter yang diberikan, apakah seseorang itu terkena diabetes atau tidak. Sistem ini dibangun menggunakan algoritma Support Vector Classifier. Sistem ini memiliki tingkat akurasi sebesar 96% pada data testing dan 95% pada data training. Meskipun memiliki tingkat akurasi yang cukup tinggi, masih dimungkinkan juga terjadinya kesalahan prediksi. Dataset yang diguakan pada sistem ini, dapat diunduh pada link berikut : [KLIK DISINI](https://www.kaggle.com/datasets/iammustafatz/diabetes-prediction-dataset/data)
+
+## Mulai Bekerja
+
+1. Kloning Repositori Ini
+
+```
+git clone https://github.com/iqbalpurba26/Prediksi-Diabetes.git
+```
+
+2. Persiapan Lingkungan
+
+```
+conda create --name main-ds python=3.10
+conda activate main-ds
+pip install joblib streamlit pandas numpy sklearn
+```
+
+3. Jalankan Streamlit
+
+```
+streamlit run stream_diabetes.py
+```
+
+## Screenshot Tampilan Halaman
+
+![Sebelum Mengisi Informasi](screenshot/before.png)
+
+![Setelah Mengisi Informasi](screenshot/after.png)
---
+++ dashboard/stream_diabetes.py
@@ -0,0 +1,67 @@
+import joblib
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# Load Model
+# diabetes_model = pickle.load(open('../diabetes_model.sav', 'rb'))
+diabetes_model = joblib.load("../diabetes_model.sav")
+df = pd.read_csv('diabets_dataset_clean.csv')
+
+X = df.drop(columns='diabetes', axis=1)
+scaler = StandardScaler()
+
+
+st.set_page_config(
+    page_title="DiabetaKu",
+    layout="wide",
+    page_icon="asset/diabetes_icon.png"
+)
+# Title Website
+st.title('DiabetaKu : Aplikasi Prediksi Diabetes')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    Gender = st.text_input('Gender (0 : Female, 1 : Male) : ')
+
+with col1:
+    Age = st.text_input("Your Age :")
+
+with col1:
+    Hipertension = st.text_input('Have hipertension? (1 : yes, 0 : no) : ')
+
+with col1:
+    Heart_disease = st.text_input('Have heart disease? (1 : yes, 0 : no) : ')
+
+with col2:
+    Smoking_history = st.text_input(
+        "Are You Smooking? (-1 : No Info, 0 : never, 1 : former, 2 : current, 3 : not current, 4 : ever) : ")
+with col2:
+    bmi = st.text_input('Your BMI : ')
+
+with col2:
+    HbA1c_level = st.text_input("Level Hemoglobin A1c : ")
+
+with col2:
+    Blood_glucose = st.text_input('Level Blood Glucose : ')
+
+input_data = pd.to_numeric([Gender, Age, Hipertension, Heart_disease,
+                           Smoking_history, bmi, HbA1c_level, Blood_glucose], errors='coerce')
+input_data_array = np.array(input_data)
+input_reshape = input_data_array.reshape(1, -1)
+scaler.fit(X)
+std_data = scaler.transform(input_reshape)
+
+diabetes_diagnosis = "BELUM MELENGKAPI DATA"
+
+if st.button('Prediction'):
+    diabetes_prediction = diabetes_model.predict(std_data)
+    if (diabetes_prediction[0] == 0):
+        diabetes_diagnosis = "PASIEN TIDAK TERKENA DIABETS"
+    else:
+        diabetes_diagnosis = "PASIEN TERKENA DIABETES"
+
+st.subheader("Hasil Prediksi Adalah")
+st.success(diabetes_diagnosis)
